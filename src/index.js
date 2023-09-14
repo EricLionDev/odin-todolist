d0eaedc5c3eb66e66e0da9794696079efe24489c
EricLionDev <ericfreisleben@gmail.com>
2023-09-14 00:19:29 -0400

    more mobile skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import "./style.css";
+import { renderMenu } from "./menu";
+export const contentDivElem = document.querySelector(".menu-sidebar");
---
+++ src/menu.js
@@ -0,0 +1,3 @@
+import { contentDivElem } from ".";
+
+export function renderMenu() {}
