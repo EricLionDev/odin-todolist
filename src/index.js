631463b99077027a6f31aa3aed1166b4c584af68
EricLionDev <ericfreisleben@gmail.com>
2023-11-19 14:52:14 -0500

    fixed localstorage functionality

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ export default function handleNewTask() {
   let task = new create_task();
   return task;
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const button = document.getElementById("new-task-btn");
+  if (button) {
+    button.addEventListener("click", handleNewTask);
+  }
+});
--- src/tasks.js
+++ src/tasks.js
@@ -1,12 +1,12 @@
 import list from "./tasklist";
 var tasknums = 0;
-var newTaskTitle = document.getElementById("new-task-title");
-var newTaskDescription = document.getElementById("new-task-description");
-var newTaskDate = document.getElementById("new-task-date");
-var newTaskList = document.getElementById("new-task-list");
 
 export default class create_task {
   constructor() {
+    var newTaskTitle = document.getElementById("new-task-title");
+    var newTaskDescription = document.getElementById("new-task-description");
+    var newTaskDate = document.getElementById("new-task-date");
+    var newTaskList = document.getElementById("new-task-list");
     this.id = tasknums;
     this.name = newTaskTitle.value;
     this.description = newTaskDescription.value;
@@ -15,7 +15,21 @@ export default class create_task {
     tasknums++;
 
     list.push(this);
-    localStorage.setItem(tasklist, JSON.stringify(list));
+    localStorage.setItem("tasklist", JSON.stringify(list));
     console.log(this.name, this.description);
+
+    this.task_form_reset();
+  }
+
+  task_form_reset() {
+    var newTaskTitle = document.getElementById("new-task-title");
+    var newTaskDescription = document.getElementById("new-task-description");
+    var newTaskDate = document.getElementById("new-task-date");
+    var newTaskList = document.getElementById("new-task-list");
+
+    newTaskTitle.value = "";
+    newTaskDescription.value = "";
+    newTaskDate.value = "";
+    newTaskList.value = "";
   }
 }
